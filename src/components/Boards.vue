a41acacb5d728f26916ce17ca12a9a7e112b5748
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-05-25 12:01:24 +0200

    Basic css divs and ugly borders
    Signed-off-by: Simone Zabberoni <simone.zabberoni@gmail.com>

--- src/components/Boards.vue
+++ src/components/Boards.vue
@@ -11,21 +11,23 @@
         </select>
         <hr>
        
-        <div v-for="list in $store.state.lists" :key="list.name">
+        <div v-bind:style="{ 'border-color': 'red', 'border-style':'solid' }">
+          <div v-for="list in $store.state.lists" :key="list.name"  v-bind:style="{ 'border-color': 'blue', 'border-style':'solid' }">
             <h3>{{ list.name }}</h3>
-            <div v-for="card in list['cards']" :key="card.name" > 
-                <div>
-                    <div v-bind:style="{ 'float': 'left' }" ><h4>{{ card.name  }} </h4></div>
-                    <div v-for="label in card['labels']" :key="label.name" >
+            <div v-for="card in list['cards']" :key="card.name" v-bind:style="{ 'border-color': 'purple', 'border-style':'solid' }"> 
+                <div v-bind:style="{ 'float': 'left', 'border-style':'solid' }">
+                    <div v-bind:style="{ 'float': 'left' }"><h4>{{ card.name  }}</h4></div>
+                    <div v-bind:style="{ 'float': 'left' }" v-for="label in card['labels']" :key="label.name" >
                         
                         <div v-bind:style="{ 'background-color': $store.state.labelColor[label.color], 'color': 'white', 'padding': '4px', 
-                            'margin': '2px', 'border-radius': '5px', 'float': 'left' } " >{{ label.name  }} </div>
+                            'margin': '2px', 'border-radius': '5px','float': 'left' } " >{{ label.name  }} </div>
                     </div>
                 </div>
-                <div v-bind:style="{ 'text-align': 'left' }"> <pre>{{  card.desc  }}</pre> </div>
+                <div v-bind:style="{ 'text-align': 'left', 'clear':'both','float':'none' }"> <pre>{{  card.desc  }}</pre> </div>
             </div>
      
             <hr>
+          </div>
         </div>
 
     </div>
